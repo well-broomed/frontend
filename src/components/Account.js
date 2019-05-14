a7faf54d21cae77d83abcea8e4e173a00d9780e5
Kevin Tena <kevten09@gmail.com>
2019-05-14 16:44:19 -0500

    Set input form to send email to mailgun to send invite

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,26 +4,58 @@ import React from 'react';
 import {connect} from 'react-redux';
 // Router
 import {withRouter} from 'react-router-dom';
+// Axios
+import axios from 'axios';
+
 
 class Account extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
-
+        this.state = {
+            email:''
+        };
     }
+    
+    handleInputChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
 
 
-    sendEmail = () => {
+    sendEmail = async (e) => {
+        // if(!this.state.email)
+        //     return;
+        e.preventDefault();
+        console.log('sending email');
+        const backendUrl = process.env.backendURL|| 'http://localhost:5000';
+        try {
+           const res = await axios.post(`${backendUrl}/api/invites/`,this.state.email);
+           console.log(res);
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
 
     render(){
         return (
             <div>
-                <button
+            <form>
+                <input
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    placeholder="Assistant Email"
+                    type="text"
+                    name="email"
+                />
+                <button 
+                type="submit"
                 onClick={this.sendEmail}
                 >
                     Send Email
                 </button>
+            </form>
             </div>
         )
     }
