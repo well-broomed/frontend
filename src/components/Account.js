03e1f2edb35e4affd4aa393a4d7865b28907abae
Kevin Tena <kevten09@gmail.com>
2019-05-14 18:10:45 -0500

    Moved sending invite function over to partners page instead of account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,58 +4,18 @@ import React from 'react';
 import {connect} from 'react-redux';
 // Router
 import {withRouter} from 'react-router-dom';
-// Axios
-import axios from 'axios';
 
 
 class Account extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            email:''
         };
-    }
-    
-    handleInputChange = (event) => {
-        this.setState({
-          [event.target.name]: event.target.value
-        });
-      }
-
-
-    sendEmail = async (e) => {
-        // if(!this.state.email)
-        //     return;
-        e.preventDefault();
-        console.log('sending email');
-        const backendUrl = process.env.backendURL|| 'http://localhost:5000';
-        try {
-           const res = await axios.post(`${backendUrl}/api/invites/`,this.state.email);
-           console.log(res);
-        }
-        catch (err) {
-            console.log(err);
-        }
     }
 
     render(){
         return (
             <div>
-            <form>
-                <input
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    placeholder="Assistant Email"
-                    type="text"
-                    name="email"
-                />
-                <button 
-                type="submit"
-                onClick={this.sendEmail}
-                >
-                    Send Email
-                </button>
-            </form>
             </div>
         )
     }
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -4,19 +4,70 @@ import React from 'react';
 import {connect} from 'react-redux';
 // Router
 import {withRouter} from 'react-router-dom';
+// Axios
+import axios from 'axios';
 
 class Partners extends React.Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+          email:''
+        };
 
     }
+
+    handleInputChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+    sendEmail = async (e) => {
+      // if(!this.state.email)
+      //     return;
+      e.preventDefault();
+      console.log('sending email');
+
+      let token = localStorage.getItem('jwt');
+      let userInfo = localStorage.getItem('userInfo');
+
+      let options = {
+          headers: {
+              Authorization: `Bearer ${token}`,
+              'user-info': userInfo,
+          }   
+      }
+
+      let body = {
+          cleaner_email: this.state.email
+      }
+      
+      const backendUrl = process.env.backendURL|| 'http://localhost:5000';
+      try {
+         const res = await axios.post(`${backendUrl}/api/invites/`, body, options);
+      }
+      catch (err) {
+          console.log(err);
+      }
+  }
 
     render(){
         return (
             <div>
-
-
+              <form>
+                <input
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    placeholder="Assistant's Email"
+                    type="text"
+                    name="email"
+                />
+                <button 
+                type="submit"
+                onClick={this.sendEmail}
+                >
+                    Send Invite
+                </button>
+            </form>
             </div>
         )
     }
