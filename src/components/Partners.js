3abe6e9a779f69880e55104e44414e16614fa541
Kevin Tena <kevten09@gmail.com>
2019-05-15 15:16:59 -0500

    Added Partners h2 headline

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 //Material-UI
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
 
 class Partners extends React.Component {
     constructor(props){
@@ -56,19 +57,22 @@ class Partners extends React.Component {
     render(){
         return (
             <div>
-                <TextField
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    placeholder="Assistant's Email"
-                    type="text"
-                    name="email"
-                />
-                <Button
-                type="submit"
-                onClick={this.sendEmail}
-                >
-                    Send Invite
-                </Button>
+              <Typography variant='h2'>Partners</Typography>
+              <div>
+                  <TextField
+                      value={this.state.email}
+                      onChange={this.handleInputChange}
+                      placeholder="Assistant's Email"
+                      type="text"
+                      name="email"
+                  />
+                  <Button
+                  type="submit"
+                  onClick={this.sendEmail}
+                  >
+                      Send Invite
+                  </Button>
+                </div>
             </div>
         )
     }
