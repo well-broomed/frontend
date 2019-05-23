81c06e01fcfc4bf5efa0e8519b17d758bb12a9c9
Kevin Tena <kevten09@gmail.com>
2019-05-23 10:01:55 -0500

    Created reducer to add partners to state

--- src/reducers/propertyReducer.js
+++ src/reducers/propertyReducer.js
@@ -1,10 +1,11 @@
-import { FETCHING_PROPERTIES, PROPERTIES_FETCHED, PROPERTY_ADDED, CLEANERS_FETCHED, CLEANER_UPDATED } from '../actions';
+import { FETCHING_PROPERTIES, PROPERTIES_FETCHED, PROPERTY_ADDED, CLEANERS_FETCHED, CLEANER_UPDATED, PARTNERS_FETCHED, FETCHING_PARTNERS } from '../actions';
 
 const initialState = {
 	properties: null,
 	refreshProperties: false,
 	cleaners: null,
 	refreshCleaners: false,
+	partners: null
 };
 
 const propertyReducer = (state = initialState, action) => {
@@ -21,7 +22,10 @@ const propertyReducer = (state = initialState, action) => {
 
 		case CLEANER_UPDATED:
 			return {...state, refreshCleaners: true}
-
+		
+		case PARTNERS_FETCHED:
+			return {...state, partners: action.payload};
+		
 		default:
 			return state;
 	}
