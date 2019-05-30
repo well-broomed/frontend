e193a5c8eed7da4d52f39df0985616028fe620fa
iAmAdamReid <adam@adamreid.me>
2019-05-30 16:35:42 -0700

    store proper returned user profile

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,14 +2,15 @@ import { USER_CHECKED } from '../actions';
 
 const initialState = {
 	userInfo: null,
-	userChecked: null
+	userChecked: null,
+	currentUser: null,
 };
 
 const authReducer = (state = initialState, action) => {
 	switch (action.type) {
 		// example action
 		case USER_CHECKED:
-			return { ...state, userInfo: action.payload, userChecked: true };
+			return { ...state, userInfo: action.payload.userInfo, userChecked: true, currentUser: action.payload.user };
 
 		default:
 			return state;
