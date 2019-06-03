082f9f94ccee2e40ecba01151f81027647a8257d
Kevin Tena <kevten09@gmail.com>
2019-06-03 18:55:51 -0500

    Implemented a tab selection system for upcoming and past reports that can be swiped, added a circle progress bar for the percentages, and started work on the properties and partners reports

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,34 +1,85 @@
 // React
 import React from 'react';
 // Redux
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 // Router
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+//SwipeableViews
+import SwipeableViews from 'react-swipeable-views';
+//Circular Progress Bar
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+// Material-UI
+import {
+	Typography,
+	Tabs,
+	Tab,
+	AppBar,
+	makeStyles,
+    Paper,
+    withTheme,
+} from '@material-ui/core';
 
 class Reports extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {};
+	constructor(props) {
+		super(props);
+		this.state = {
+			tabValue: 0
+		};
+	}
 
-    }
+	handleTabChange = (event, newValue) => {
+		this.setState({ tabValue: newValue });
+	};
 
-    render(){
-        return (
-            <div></div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<Typography variant="h2">Reports</Typography>
+				<AppBar position="static" color="default">
+					<Tabs
+						value={this.state.tabValue}
+						onChange={this.handleTabChange}
+						indicatorColor="primary"
+						textColor="primary"
+                        centered
+					>
+						<Tab label="Upcoming" />
+						<Tab label="Past" />
+					</Tabs>
+				</AppBar>
+				<SwipeableViews
+                    axis={this.props.theme.direction === 'rtl' ? 'x-reverse': 'x'}
+                    index={this.state.tabValue}
+                    onChangeIndex={this.handleChangeIndex}
+                >
+                <Typography component="div" dir={this.props.theme.direction}> 
+                <CircularProgressbar value={33} text={33 +'%'} strokeWidth={8}
+                    styles={buildStyles({width: '50%' })}
+                />
+                <Typography variant='h4' align='center'> Overall Completion </Typography>
+                <Typography variant='h5'> Properties </Typography>
+                <Typography variant='h5'> Partners </Typography>
+                 </Typography>
+                <Typography dir={this.props.theme.direction}> Past
+                </Typography>
+                </SwipeableViews>
+			</div>
+		);
+	}
 }
-
 
 const mapStateToProps = state => {
-    return {
-        // state items
-    }
-}
+	return {
+		// state items
+	};
+};
 
-export default withRouter(connect(mapStateToProps, {
-    // actions
-    
-})(Reports))
-
-
+export default withRouter(
+	connect(
+		mapStateToProps,
+		{
+			// actions
+		}
+	)(withTheme(Reports))
+);
