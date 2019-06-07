281e1bf2ea5ebf8f31e7001a403b54fc4971ba66
Kevin Tena <kevten09@gmail.com>
2019-06-07 17:05:35 -0500

    Components/reports (#26)
    * Implemented a tab selection system for upcoming and past reports that can be swiped, added a circle progress bar for the percentages, and started work on the properties and partners reports
    
    * Implemented styles to style progress bar better
    
    * Styled properties progress bars and used CSS to align each item
    
    * Styled partners progress bars using the same code for properties
    
    * Added the same styling for past tasks tab as well and put in dividiers to create more breathing space
    
    * Changed tab values

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,41 +1,255 @@
 // React
 import React from 'react';
 // Redux
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 // Router
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+//SwipeableViews
+import SwipeableViews from 'react-swipeable-views';
+//Circular Progress Bar
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+// Material-UI
+import {
+	Typography,
+	Tabs,
+	Tab,
+	AppBar,
+	withStyles,
+	Paper,
+	withTheme,
+    Divider
+} from '@material-ui/core';
+
+const styles = {
+	progressbar: {
+		width: '50%',
+		display: 'flex',
+        margin: 'auto',
+        height: '200px'
+	},
+	progressBarReports: {
+		width: '75%',
+		display: 'flex'
+	},
+	appbar: {
+		marginTop: '15px'
+    },
+    divider: {
+        margin: '15px 0'
+    },
+	reports: {
+		marginTop: '20px'
+	},
+	progresses: {
+		display: 'flex',
+		flexWrap: 'wrap',
+        justifyContent: 'center',
+        margin: '10px 0'
+	},
+	singleProgress: {
+		width: '33%',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	}
+};
 
 class Reports extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tabValue: 0
+		};
+	}
 
-    componentDidMount(){
+	componentDidMount(){
         if(!localStorage.getItem('jwt')){
 			this.props.history.replace('/');
 		}
     }
 
-    constructor(props){
-        super(props);
-        this.state = {};
+	handleTabChange = (event, newValue) => {
+		this.setState({ tabValue: newValue });
+	};
 
-    }
+	render() {
+		const { classes } = this.props;
+		const dummyProperties = [
+			{
+				property_name: 'Sears Tower',
+				property_tasks: 50
+			},
+			{
+				property_name: 'Hogwarts',
+				property_tasks: 33
+			},
+			{
+				property_name: 'Boardwalk',
+				property_tasks: 49
+			},
+			{
+				property_name: 'Notre Dame',
+				property_tasks: 91
+			}
+		];
 
-    render(){
-        return (
-            <div></div>
-        )
-    }
+		const dummyPartners = [
+			{
+				partner_name: 'Fuzzy Dunlop',
+				partner_tasks: 25
+			},
+			{
+				partner_name: 'Carlos Slim',
+				partner_tasks: 91
+			},
+			{
+				partner_name: 'Bill Gates',
+				partner_tasks: 63
+			},
+			{
+				partner_name: 'Marlo Stanfield',
+				partner_tasks: 17
+			}
+		];
+
+		return (
+			<div>
+				<Typography variant="h2">Reports</Typography>
+				<AppBar position="static" color="default" className={classes.appbar}>
+					<Tabs
+						value={this.state.tabValue}
+						onChange={this.handleTabChange}
+						indicatorColor="primary"
+						textColor="primary"
+						centered
+					>
+						<Tab label="Before Stay" />
+						<Tab label="During Stay" />
+						<Tab label="After Stay" />
+					</Tabs>
+				</AppBar>
+				<SwipeableViews
+					axis={this.props.theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+					index={this.state.tabValue}
+					onChangeIndex={this.handleChangeIndex}
+					className={classes.reports}
+				>
+					<Typography component="div" dir={this.props.theme.direction}>
+                    <Typography variant="h4" align="center">
+							{' '}
+							Overall Completion{' '}
+						</Typography>
+						<div className={classes.progressbar}>
+							<CircularProgressbar value={33} text={33 + '%'} />
+						</div>
+                        <Divider className={classes.divider}/>
+						<Typography variant="h4" align="center"> Properties </Typography>
+						<div className={classes.progresses}>
+							{dummyProperties.map(property => {
+								return (
+									<div className={classes.singleProgress}>
+										<div className={classes.progressBarReports}>
+											<CircularProgressbar
+												value={property.property_tasks}
+												text={`${property.property_tasks}%`}
+											/>
+										</div>
+										<Typography variant="h6" align="center">
+											{' '}
+											{property.property_name}{' '}
+										</Typography>
+									</div>
+								);
+							})}
+						</div>
+                        <Divider className={classes.divider}/>
+						<Typography variant="h4" align="center"> Partners </Typography>
+						<div className={classes.progresses}>
+							{dummyPartners.map(partner => {
+								return (
+									<div className={classes.singleProgress}>
+										<div className={classes.progressBarReports}>
+											<CircularProgressbar
+												value={partner.partner_tasks}
+												text={`${partner.partner_tasks}%`}
+											/>
+										</div>
+										<Typography variant="h6" align="center">
+											{' '}
+											{partner.partner_name}{' '}
+										</Typography>
+									</div>
+								);
+							})}
+						</div>
+					</Typography>
+					<Typography dir={this.props.theme.direction}>
+                    <Typography variant="h4" align="center">
+							{' '}
+							Overall Completion{' '}
+						</Typography>
+                    <div className={classes.progressbar}>
+							<CircularProgressbar value={33} text={33 + '%'} />
+						</div>
+                    <Divider className={classes.divider}/>
+                    <Typography variant="h5" align='center'> Properties </Typography>
+						<div className={classes.progresses}>
+							{dummyProperties.map(property => {
+								return (
+									<div className={classes.singleProgress}>
+										<div className={classes.progressBarReports}>
+											<CircularProgressbar
+												value={property.property_tasks}
+												text={`${property.property_tasks}%`}
+											/>
+										</div>
+										<Typography variant="h6" align="center">
+											{' '}
+											{property.property_name}{' '}
+										</Typography>
+									</div>
+								);
+							})}
+						</div>
+                        <Divider className={classes.divider}/>
+						<Typography variant="h5" align="center"> Partners </Typography>
+						<div className={classes.progresses}>
+							{dummyPartners.map(partner => {
+								return (
+									<div className={classes.singleProgress}>
+										<div className={classes.progressBarReports}>
+											<CircularProgressbar
+												value={partner.partner_tasks}
+												text={`${partner.partner_tasks}%`}
+											/>
+										</div>
+										<Typography variant="h6" align="center">
+											{' '}
+											{partner.partner_name}{' '}
+										</Typography>
+									</div>
+								);
+							})}
+						</div>
+					</Typography>
+				</SwipeableViews>
+			</div>
+		);
+	}
 }
-
 
 const mapStateToProps = state => {
-    return {
-        // state items
-    }
-}
+	return {
+		// state items
+	};
+};
 
-export default withRouter(connect(mapStateToProps, {
-    // actions
-    
-})(Reports))
-
-
+export default withRouter(
+	connect(
+		mapStateToProps,
+		{
+			// actions
+		}
+	)(withStyles(styles, { withTheme: true })(Reports))
+);
