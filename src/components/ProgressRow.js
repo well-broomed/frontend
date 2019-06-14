52b8d8f276979ce05156a94869ca34236663bb3f
Max <maxkajiwara@gmail.com>
2019-06-14 06:35:31 -0700

    Add property link

--- src/components/ProgressRow.js
+++ src/components/ProgressRow.js
@@ -1,6 +1,9 @@
 // React
 import React from 'react';
 
+// Router
+import { Link } from 'react-router-dom';
+
 // Components
 import { ProgressCircle } from '../components';
 
@@ -13,13 +16,19 @@ import { Typography } from '@material-ui/core';
 const ProgressRow = props => {
 	const { property, even } = props;
 
+	const propertyInfo = property.recent || property.current || property.upcoming;
+
 	return (
 		<PropertyRowContainer style={{ background: even ? 'inherit' : 'white' }}>
-			<PropertyName variant="h6" align="center">
-				{property.recent.property_name ||
-					property.current.property_name ||
-					property.upcoming.property_name}
-			</PropertyName>
+			<PropertyLink to={`/properties/${propertyInfo.property_id}`}>
+				<PropertyName
+					variant="h6"
+					align="center"
+					style={{ letterSpacing: '.03rem' }}
+				>
+					{propertyInfo.property_name}
+				</PropertyName>
+			</PropertyLink>
 
 			<PropertyRow>
 				<ProgressCircle
@@ -44,11 +53,18 @@ export default ProgressRow;
 
 const PropertyRowContainer = styled.div`
 	width: 100%;
+	/* padding: 16px 0 6px; */
+`;
+
+const PropertyLink = styled(Link)`
+	color: inherit;
+	text-decoration: none;
 `;
 
 const PropertyRow = styled.div`
 	display: flex;
 	width: 100%;
+	/* margin: 0 0 -20px; */
 `;
 
 const PropertyName = styled(Typography)`
